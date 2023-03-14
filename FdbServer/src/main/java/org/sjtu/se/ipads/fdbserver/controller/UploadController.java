5994fbe649fd85aad206a06cb9ca4617abb173c7
Szy0127 <1799853523@qq.com>
2023-03-14 16:25:16 +0800

    feat: add upload_multi service

--- FdbServer/src/main/java/org/sjtu/se/ipads/fdbserver/controller/UploadController.java
+++ FdbServer/src/main/java/org/sjtu/se/ipads/fdbserver/controller/UploadController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 @RestController
 public class UploadController {
@@ -18,26 +19,20 @@ public class UploadController {
     @Resource
     UploadService uploadService;
 
-
-//    @RequestMapping("/uploadData")
-//    public boolean uploadData(
-//            @RequestParam("time_stamp") long time_stamp,
-//            @RequestParam("car_id") int car_id,
-//            @RequestParam("x") double x,
-//            @RequestParam("y") double y,
-//            @RequestParam("r") double r,
-//            @RequestParam("vx") double vx,
-//            @RequestParam("vy") double vy,
-//            @RequestParam("vr") double vr,
-//            @RequestParam("image") String img
-//
-//    ){
-//        return carService.uploadData(time_stamp,car_id,x,y,r,vx,vy,vr,img);
-//    }
-
     @PostMapping("/uploadData_v2")
     public boolean uploadData_v2(@RequestBody JSONObject carData) {
         return uploadService.save(carData);
+    }
+
+    @PostMapping("/uploadData_multi")
+    public int uploadData_multi(@RequestBody List<JSONObject> carDatas) {
+        int count = 0;
+        for(JSONObject data:carDatas){
+            if(uploadService.save(data)){
+                count++;
+            }
+        }
+        return count;
     }
 
 
